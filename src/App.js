446a65057f46ae5020f606124c96a2a93eceac92
wozszym <wozszym@gmail.com>
2021-11-29 21:01:41 +0000

    Init

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react'
+const App = () => (
+  <div>
+    <h1>Beautiful front end for famous how do I feel portal</h1>
+  </div>
+)
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import ReactDOM from 'react-dom'
+import App from './App'
+
+/*
+const cors = require('cors')
+app.use(cors())
+*/
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
