f88b9ac28654e37084bd504776a5853d021b914c
wozszym <wozszym@gmail.com>
2021-11-29 21:10:11 +0000

    Update welcome screen

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from 'react'
 const App = () => (
   <div>
     <h1>Beautiful front end for famous how do I feel portal</h1>
-  </div>
+    <br></br>
+    <h2>There will be many interesting stuff here </h2>
+  </div >
 )
 
 export default App
