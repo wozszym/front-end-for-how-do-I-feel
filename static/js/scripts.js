86daa4327bafff6c177494206a6785e31be257e8
wozszym <wozszym@gmail.com>
2021-12-23 16:00:25 +0000

    Current version

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,99 +1,162 @@
-
+const debug = false
 
 // Set value for a range element
 function setValueFroRange(event) {
-    console.log(event)
+    event.preventDefault();
+    if (debug) console.log(event)
     const x = document.getElementById("generalFeeling").value;
-    console.log('Range value = ' + x)
+    if (debug) console.log('Range value = ' + x)
     document.getElementById("generalFeelingRangeValue").innerHTML = x;
 }
 
 const elem = document.getElementById("generalFeeling");
 elem.addEventListener("change", setValueFroRange);
+elem.addEventListener("input", setValueFroRange);
 
-///////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////
-//https://cloud.google.com/blog/products/maps-platform/quick-map-layer-visualizations-geojson-and-georss
-//https://developers.google.com/maps/documentation/javascript/markers#maps_icon_simple-javascript
-//https://stackoverflow.com/questions/30574959/set-fill-color-marker-google-map
-function pinSymbol(color) {
-    return {
-        path: 'M 0,0 C -2,-20 -10,-22 -10,-30 A 10,10 0 1,1 10,-30 C 10,-22 2,-20 0,0 z',
-        fillColor: color,
-        fillOpacity: 1,
-        strokeColor: '#000',
-        strokeWeight: 2,
-        scale: 1
-    };
+function parse(data) {
+    if (data) {
+        return JSON.parse(data)
+    }
+    return []
 }
 
-function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 15,
-        center: { lat: 51.50093098544679, lng: -0.015221855144604184 }
-    });
+// handle form. Probably TODO: remove when back end will handle forms
+function addResultToCache(newItem) {
+    let items = parse(localStorage.getItem('data'))
+    if (debug) console.log(items)
+    if (items) {
+        items.push(newItem)
+    }
+    else {
+        items = [newItem]
+    }
+    if (debug) console.log(items)
+    localStorage.setItem('data', JSON.stringify(items))
+}
 
-    var tempObject =
-    {
-        "type": "Feature",
-        "geometry": {
-            "type": "Point",
-            "coordinates": [-0.015221, 51.500930]
-        },
-        "properties": {
-            "name": "Initial one",
+function saveFormResultToLocalStorage(event) {
+    event.preventDefault();
+
+    const sex = document.querySelector('input[name="sex"]:checked').value;
+    const country = document.getElementById('country').value
+    const postCode = document.getElementById('postcode').value
+    const generalFeeling = document.getElementById('generalFeeling').value
+
+    const formResult = {
+        sex,
+        country,
+        postCode,
+        generalFeeling
+    }
+    addResultToCache(formResult)
+}
+
+const mainForm = document.getElementById("main-form");
+mainForm.addEventListener("submit", saveFormResultToLocalStorage);
+
+
+// return bucket counts and labels
+// Function is not very efective as that logic would be most likely in backend
+function bucketCounts(data, min, max, nBuckets) {
+    const step = Math.round((max - min) / nBuckets)
+
+    let buckets = []
+    let last = 0
+    for (let i = 0; i < nBuckets - 1; i++) {
+        buckets.push(step + last)
+        last += step
+    }
+
+    let labels = []
+    last = 0
+    for (let i = 0; i < nBuckets - 1; i++) {
+        labels.push(`[${last}-${buckets[i]})`)
+        last += step
+    }
+    labels.push(`[${last}-${max}]`)
+
+    let counts = new Array(nBuckets).fill(0)
+    data.forEach(item => {
+        let idx = buckets.findIndex(bucketTreshold => bucketTreshold > item)
+        if (idx == -1) { // move to last bucket
+            idx = nBuckets - 1
         }
-    }
+        counts[idx]++;
+    })
 
-    map.data.addGeoJson(tempObject);
+    console.log({ buckets, labels, counts })
 
-
-    // 2nd way
-
-    const positions = { lat: 51.50, lng: -0.016 }
-    let marker = new google.maps.Marker({
-        position: positions,
-        title: "For accessability",
-        label: "Hello Mr Pawel",
-        icon: pinSymbol('green')
-    });
-
-    marker.setMap(map)
-}
-/*
-// https://developers.google.com/maps/documentation/javascript/importing_data
-let map;
-
-function initMap() {
-    console.log('I am being called')
-    map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 2,
-        center: new google.maps.LatLng(2.8, -187.3),
-        mapTypeId: "terrain",
-    });
-
-    // Create a <script> tag and set the USGS URL as the source.
-    const script = document.createElement("script");
-
-    // This example uses a local copy of the GeoJSON stored at
-    // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
-    script.src =
-        "https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js";
-    document.getElementsByTagName("head")[0].appendChild(script);
+    return { buckets, labels, counts }
 }
 
-// Loop through the results array and place a marker for each
-// set of coordinates.
-const eqfeed_callback = function (results) {
-    for (let i = 0; i < results.features.length; i++) {
-        const coords = results.features[i].geometry.coordinates;
-        const latLng = new google.maps.LatLng(coords[1], coords[0]);
+function getGeneralFeeling(data) {
+    return data.map(item => item.generalFeeling)
+}
 
-        new google.maps.Marker({
-            position: latLng,
-            map: map,
-        });
-    }
-};
-*/
+// Display simple histogram
+function displaySimpleHistogram() {
+    const ctx = document.getElementById('histogram');
+    const dataFromStorage = parse(localStorage.getItem('data'))
+    const bucketData = bucketCounts(getGeneralFeeling(dataFromStorage), 0, 100, 10)
+
+    const n = dataFromStorage.length
+    document.getElementById("numberOfDataPoints").innerHTML = n
+
+    const myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: bucketData.labels,
+            datasets: [{
+                label: '# of Votes',
+                data: bucketData.counts,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                    'rgba(130, 159, 64, 0.2)',
+                    'rgba(180, 159, 64, 0.2)',
+                    'rgba(190, 159, 64, 0.2)',
+                    'rgba(230, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)',
+                    'rgba(130, 159, 64, 1)',
+                    'rgba(180, 159, 64, 1)',
+                    'rgba(190, 159, 64, 1)',
+                    'rgba(230, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
+
+function handleSubmitButton() {
+    let histogramBox = document.getElementById("histogramBox");
+    histogramBox.classList.remove("noDisplay");
+
+    // remove previously created chart
+    $("#histogram").remove();
+    $("#histogramParent").append('<canvas id="histogram" width="400" height="230"></canvas>');
+
+    displaySimpleHistogram()
+}
+
+
+const submitButton = document.getElementById("mainSubmitButton");
+submitButton.addEventListener("click", handleSubmitButton);
